0636c23d1d42b9cda9238fa395bdc300caf98644
Shivkumar Verma <113050680+KurmiShiv@users.noreply.github.com>
2023-09-13 12:12:33 +0530

    Update settings.py
    Debug= true

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9aw6+)5@lagn$_sla*c1hkcagfc4h4d+qi)lng(y@annaw3u1z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
